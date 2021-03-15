5c08d77eeb8615829ff34199f11086d66c769cca
Vasylyna611 <vasylynakuzyshyn@gmail.com>
2021-03-15 17:42:57 +0000

    Sample code

---
+++ js/index.js
@@ -0,0 +1,37 @@
+//alert('Hello')
+
+//const message = 'Hello JavaScript';
+//alert(message);
+
+//let message = 'Hello JavaScript';
+//alert(message);
+//message = 'Hello World';
+//alert(message);
+
+//const userName = prompt('Enter your name');
+//alert ('Hello' + userName);
+
+//const userAge = prompt('Enter your age');
+//alert('Your age is' + userAge);
+
+const userAge = Number(prompt('Enter your age'));
+alert('Your age is' + userAge);
+
+const nextAge = userAge + 1;
+alert('Your next age should be' + nextAge);
+
+//String - ''
+//Number - 123
+//Boolean - true, false
+
+if (userAge >=18) {
+    alert('You are adult');
+} else {
+    alert('You are underaged');
+}
+
+//function add(a, b) { a = 42, b = 1
+    //const result = a + b; result = 43
+    //return result;
+//}
+
